c9e3ce2d128bbcad993730b4ca9a2164ae195b6d
nexustradesocial-a11y <nexustradesocial@gmail.com>
2026-05-24 07:42:56 -0500

    Add Docker setup, Fly.io config, dependencies, and API configuration

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+X_API_KEY = os.getenv("X_API_KEY")
+X_API_SECRET = os.getenv("X_API_SECRET")
+X_ACCESS_TOKEN = os.getenv("X_ACCESS_TOKEN")
+X_ACCESS_SECRET = os.getenv("X_ACCESS_SECRET")
+
+OPENAI_KEY = os.getenv("OPENAI_KEY")
+XAI_KEY = os.getenv("XAI_KEY")
